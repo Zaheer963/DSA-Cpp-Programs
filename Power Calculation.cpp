17e3588245639670d76114ad644e87cd859745de
Zaheer <abdulzaheer963@gmail.com>
2025-06-11 15:43:28 +0530

    power

---
+++ Power Calculation.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c=0;
+    cin>>a>>b;
+    c=pow(a,b);
+    cout<<c;
+    return 0;
+}
