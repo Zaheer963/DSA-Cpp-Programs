2aaea2994e09e06aabe49c868aabbe4f29588261
Zaheer <abdulzaheer963@gmail.com>
2025-06-12 10:23:59 +0530

    degrees

---
+++ Convert Celsius to Fahrenheit.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {  
+    int c,f;
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<<f;
+    return 0;
+}
