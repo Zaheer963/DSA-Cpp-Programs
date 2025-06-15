32ef915dc369caddd63dbf7ecfa4c55e6c24242c
Zaheer <abdulzaheer963@gmail.com>
2025-06-15 10:46:11 +0530

    check

---
+++ Leap Year.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {   
+    int n;
+    cin>>n;
+    if(n%4==0 && n%100!=0 || n%400==0)
+    {
+        cout<<"Leap Year";
+    }
+    else
+    {
+        cout<<"Not a Leap Year";
+    }
+    return 0;
+}
