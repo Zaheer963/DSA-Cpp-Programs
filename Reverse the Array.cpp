1fdcc6682f66c00d7db03b0daf2dfdc712d20a7b
Zaheer <abdulzaheer963@gmail.com>
2025-06-16 11:24:07 +0530

    reversing

---
+++ Reverse the Array.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        b[i]=a[i];
+        cout<<b[i]<<" ";
+    }
+    return 0;
+}
