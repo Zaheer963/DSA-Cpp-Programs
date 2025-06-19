763742e4c76f04d4747e76af19a53ce1ac5a21c2
Zaheer <abdulzaheer963@gmail.com>
2025-06-19 19:08:11 +0530

    missing

---
+++ Smallest Positive Missing Number.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,k=1;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            k++;
+            
+        }
+    }
+    cout<<k;
+
+    return 0;
+}
