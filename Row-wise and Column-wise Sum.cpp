40b484ec425c82359c2dc267bcd7f2034c9f1482
Zaheer <abdulzaheer963@gmail.com>
2025-06-25 18:37:47 +0530

    matrix sum

---
+++ Row-wise and Column-wise Sum.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int r,c;
+    cin>>r>>c;
+    vector<vector<int>>v;
+    for(int i=0;i<r;i++)
+    {
+        vector<int>a;
+        for(int j=0;j<c;j++)
+        {
+            int z;
+            cin>>z;
+            a.push_back(z);
+        }
+        v.push_back(a);
+    }
+    for(int i=0;i<r;i++)
+    {
+        int co=0;
+        for(int j=0;j<c;j++)
+        {
+            co+=v[i][j];
+        }
+        cout<<co<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<c;i++)
+    {
+        int ro=0;
+        for(int j=0;j<r;j++)
+        {
+            ro+=v[j][i];
+        }
+        cout<<ro<<" ";
+    }
+    return 0;
+}
