0d27ef989902c6d9e6fd14e341ce64ecc89466a8
Zaheer <abdulzaheer963@gmail.com>
2025-06-27 18:20:38 +0530

    col to row

---
+++ Exchange matrix columns.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {  
+    int r,c;
+    cin>>r>>c;
+    vector<vector<int>>v;
+    for(int i=0;i<r;i++)
+    {
+        vector<int>a;
+        for(int j=0;j<c;j++)
+        {
+            int z;
+            cin>>z;
+            a.push_back(z);
+        }
+        v.push_back(a);
+    }
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j==c-1)
+            {
+                cout<<v[i][j]<<" ";
+            }
+        }
+        for(int j=0;j<c;j++)
+        {
+            if((i<r && j!=0) && (i<r && j!=c-1)){
+            cout<<v[i][j]<<" ";
+            }
+        }
+        for(int j=0;j<c;j++)
+        {
+            if(j==0)
+            {
+                cout<<v[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
