69f7d2e5fab79a04d669257c6878f4202df11427
Zaheer <abdulzaheer963@gmail.com>
2025-07-06 12:48:30 +0530

    common elements

---
+++ Intersection of two sorted arrays.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() { 
+    int n,m;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cin>>m;
+    int b[m];
+    for(int i=0;i<m;i++)
+    {
+        cin>>b[i];
+    }
+    int i=0,j=0;
+    while(i<n && j<m)
+    {
+        if(a[i]<b[j])
+        {
+            i++;
+        }
+        else if(a[i]>b[j])
+        {
+            j++;
+        }
+        else if(a[i]==b[j])
+        {
+            cout<<a[i]<<" ";
+            i++;
+            j++;
+        }
+    }
+    return 0;
+}
